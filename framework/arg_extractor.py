7c9815c014c9cf0010a444d7c61664b2ae3599c9
DiWeng <di.w@hotmail.com>
2020-02-10 19:18:45 +0000

    number of epoch set to 100

--- framework/arg_extractor.py
+++ framework/arg_extractor.py
@@ -18,15 +18,22 @@ def get_args():
     parser = argparse.ArgumentParser(
         description='Welcome to the MLP course\'s Pytorch training and inference helper script')
 
-    parser.add_argument('--batch_size', nargs="?", type=int, default=100, help='Batch_size for experiment')
-    parser.add_argument('--continue_from_epoch', nargs="?", type=int, default=-1, help='Batch_size for experiment')
+    parser.add_argument('--batch_size', nargs="?", type=int,
+                        default=100, help='Batch_size for experiment')
+    parser.add_argument('--continue_from_epoch', nargs="?",
+                        type=int, default=-1, help='Batch_size for experiment')
     parser.add_argument('--seed', nargs="?", type=int, default=1902530,
                         help='Seed to use for random number generator for experiment')
-    parser.add_argument('--input_dim', nargs='?', type=int, default=40, help='Input dimensionality for experiment, equals to n_mfcc')
-    parser.add_argument('--hidden_dim', nargs='?', type=int, default=64, help='Hidden dimensionality for experiment')
-    parser.add_argument('--num_epochs', nargs="?", type=int, default=100, help='The experiment\'s epoch budget')
-    parser.add_argument('--num_classes', nargs="?", type=int, default=4, help='The experiment\'s output classes')
-    parser.add_argument('--num_layers', nargs="?", type=int, default=1, help='Number of BLSTM layers')
+    parser.add_argument('--input_dim', nargs='?', type=int, default=40,
+                        help='Input dimensionality for experiment, equals to n_mfcc')
+    parser.add_argument('--hidden_dim', nargs='?', type=int,
+                        default=64, help='Hidden dimensionality for experiment')
+    parser.add_argument('--num_epochs', nargs="?", type=int,
+                        default=1000, help='The experiment\'s epoch budget')
+    parser.add_argument('--num_classes', nargs="?", type=int,
+                        default=4, help='The experiment\'s output classes')
+    parser.add_argument('--num_layers', nargs="?", type=int,
+                        default=1, help='Number of BLSTM layers')
     parser.add_argument('--experiment_name', nargs="?", type=str, default="exp_1",
                         help='Experiment name - to be used for building the experiment folder')
     parser.add_argument('--use_gpu', nargs="?", type=str2bool, default=True,
