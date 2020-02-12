72accd77abc3d2b9cf3779bb68e7272903cbe0e7
di.W <di.w@hotmail.com>
2020-02-12 16:56:12 +0000

    修改fix.py 相对路径

--- framework/fix.py
+++ framework/fix.py
@@ -8,7 +8,7 @@ This .py helps fix the old checkpoint name issue.
 2) Then create a dummy class called `Timer()` in a dummy file called `timmer.py`
 3) Run this script to fix the old checkpoint files that store a 'utils.timer' that is no longer used
 """
-input_ckpt = os.path.join('/home/hongyuan/Desktop/mlp_groupcw/framework/MPC', 'mockingjay-500000.ckpt')
+input_ckpt = os.path.join('MPC/mockingjay-500000.ckpt')
 
 all_states = torch.load(input_ckpt, map_location='cpu')
 config = all_states['Settings']['Config']
