b46e238e4d7e7fe76524ba6b82932b2046f24907
Hongyuan <remremrinrinrin@gmail.com>
2020-02-12 08:09:02 -0500

    MPC included.

--- framework/data_providers.py
+++ framework/data_providers.py
@@ -12,8 +12,8 @@ import random
 import os
 from torch.utils import data
 import torch
-from apc_model import APCModel
-from utils import PrenetConfig, RNNConfig
+from APC.apc_model import APCModel
+from APC.utils import PrenetConfig, RNNConfig
 
 from tqdm import tqdm
 
@@ -29,11 +29,9 @@ class IEMOCAP(data.Dataset):
         self.fd_root = '../feature_data/' + experiment_name
         self.mode = mode
         if not os.path.exists(self.fd_root+'_' + self.mode):
-            self.compute_mspc()
-            self.compute_mfcc()
+            self.compute_feature(experiment_name)
         with open(self.fd_root+'_' + self.mode,'rb') as f:
             self.data = pickle.load(f)
-
         self.transform = transform
 
     def __len__(self):
@@ -47,11 +45,14 @@ class IEMOCAP(data.Dataset):
 
         return x, y
 
-    # 随机划分数据集
-    def compute_mfcc(self):
+    def compute_feature(self, experiment_name):
         emotion_data = {}
         random.seed(123456)
-
+        prenet_config = None
+        	
+        if experiment_name=="mpc":
+            model_path = 'MPC/mockingjay-500000.ckpt'
+            mockingjay = get_mockingjay_model(from_path=model_path)
         for emotion_name in tqdm(os.listdir(self.mfcc_datafolder)):
             temp_list = []
             if emotion_name.startswith('.'):
@@ -63,84 +64,35 @@ class IEMOCAP(data.Dataset):
                 wav_path = emotion_folder + wav_file
                 wav_sr = 16000
                 y, sr = librosa.load(path=wav_path, sr=wav_sr)
-                mfccs = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=40, hop_length=int(sr/100), n_fft=int(sr/40))
-                labels = self.emotion_classes[emotion_name]
-                temp_list.append((torch.Tensor(mfccs), labels))
-            emotion_data[emotion_name] = temp_list
+                if experiment_name=="mfcc":
+                    feature = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=40, hop_length=int(sr/100), n_fft=int(sr/40))
 
-        train_data = []
-        val_data = []
-        test_data = []
-
-        for emotion, emotion_data_list in emotion_data.items():
-            random.shuffle(emotion_data_list)
-            length = len(emotion_data_list)
-            train_data.extend(emotion_data_list[:int(length * 0.7)])
-            val_data.extend(emotion_data_list[int(length * 0.7):int(length * 0.9)])
-            test_data.extend(emotion_data_list[int(length * 0.9):])
-
-        random.shuffle(train_data)
-        random.shuffle(val_data)
-        random.shuffle(test_data)
-
-        with open(self.fd_root+'_train', 'wb') as f:
-            pickle.dump(train_data, f, protocol=4)
-        with open(self.fd_root + '_val', 'wb') as f:
-            pickle.dump(val_data, f, protocol=4)
-        with open(self.fd_root + '_test', 'wb') as f:
-            pickle.dump(test_data, f, protocol=4)
-
-    # 随机划分数据集
-    def compute_mspc(self):
-        emotion_data = {}
-        random.seed(123456)
-        prenet_config = None	
-        rnn_config = RNNConfig(input_size=80, hidden_size=512, num_layers=3, residual=True, dropout=0.)
-        pretrained_apc = APCModel(mel_dim=80, prenet_config=prenet_config, rnn_config=rnn_config).cuda()
-        pretrained_weights_path = 'bs32-rhl3-rhs512-rd0-adam-res-ts20.pt'
-        pretrained_apc.load_state_dict(torch.load(pretrained_weights_path))
-        device=torch.device(0) 
-        example_path = '/home/hongyuan/Desktop/mlp_groupcw/framework/result/result_mockingjay/mockingjay_libri_sd1337_MelBase/mockingjay-500000.ckpt'
-        mockingjay = get_mockingjay_model(from_path=example_path)
-
-        for emotion_name in tqdm(os.listdir(self.mfcc_datafolder)):
-            temp_list = []
-            if emotion_name.startswith('.'):
-                continue
-            emotion_folder = '../dataset/' + emotion_name + '/'
-            for wav_file in tqdm(os.listdir(emotion_folder)):
-                if wav_file.startswith('.'):
-                    continue
-                wav_path = emotion_folder + wav_file
-                wav_sr = 16000
-                y, sr = librosa.load(path=wav_path, sr=wav_sr)
-                mfccs = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=40, hop_length=int(sr/100), n_fft=int(sr/40))
-                #print(mfccs.shape)
-                mel=librosa.feature.melspectrogram(y=y, sr=sr,n_mels=160, n_fft=int(sr/40), hop_length=int(sr/100))
-                mel=np.expand_dims(mel, axis=0)
-                mel=np.transpose(mel, (0, 2, 1))
-                
-                mel=torch.Tensor(mel)
-                print([mel.shape[1]])
-                mel=mel.to('cpu')
-                length=[mel.shape[1]]
-                length=torch.Tensor(length)
-                length=length.to('cpu')
+                if experiment_name=="mspc":
+                    feature=librosa.feature.melspectrogram(y=y, sr=sr,n_mels=160, n_fft=int(sr/40), hop_length=int(sr/100))
+                if experiment_name=="mpc":
+                    feature=librosa.feature.melspectrogram(y=y, sr=sr,n_mels=160, n_fft=int(sr/40), hop_length=int(sr/100))
+                    feature=np.expand_dims(feature, axis=0)
+                    feature=np.transpose(feature, (0, 2, 1))
+                    feature=torch.Tensor(feature)
+                    feature=feature.to('cpu')
+                    
+                    length=[feature.shape[1]]
+                    length=torch.Tensor(length)
+                    length=length.to('cpu')
 
                 #_, mel = pretrained_apc.forward(mel, length)
                                 # reps.shape: (batch_size, seq_len, hidden_size)
-                mel = mockingjay.forward(spec=mel, all_layers=False, tile=True)
-                #mel=mel[-1,-1,:,:]
-                mel=mel[-1,:,:]
+                    feature = mockingjay.forward(spec=feature, all_layers=False, tile=True)
+                    #mel=mel[-1,-1,:,:]
+                    feature=feature[-1,:,:]
                 
-                mel=mel.transpose(1, 0)
-                mel=mel.to("cpu")
-                mel=mel.detach() 
+                    feature=feature.transpose(1, 0)
+                    feature=feature.to("cpu")
+                    feature=feature.detach() 
    
-                print(mel.shape)
                 labels = self.emotion_classes[emotion_name]
      
-                temp_list.append((mel, labels))
+                temp_list.append((feature, labels))
             emotion_data[emotion_name] = temp_list
 
         train_data = []
@@ -174,6 +126,7 @@ class IEMOCAP(data.Dataset):
     def test(self):
 
         return [temp[0] for temp in self.data]
+
 if __name__ == '__main__':
 
     ama = IEMOCAP('mpc',mode='train')
